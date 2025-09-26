dc75f9cf958f7adaa821447bd7f7cb5504c6bdbb
Arteverso <163326707+CrisMov3@users.noreply.github.com>
2025-09-26 16:19:40 -0500

    Update script.js
    Se agrega función para el modal

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Bienvenido a STROKBIG 🚀");
+document.addEventListener("DOMContentLoaded", () => {
+  const modalLinks = document.querySelectorAll(".link.proximamente");
+  const modal = document.getElementById("modal");
+  const closeModal = document.getElementById("closeModal");
+
+  modalLinks.forEach(link => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      modal.style.display = "flex";
+    });
+  });
+
+  closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
